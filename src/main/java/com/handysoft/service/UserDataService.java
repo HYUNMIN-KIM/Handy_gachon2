8feb24988b4aa20919e92f25d780c604a3e70def
partner_jun <partner_jun@live.co.kr>
2016-02-05 15:11:35 +0900

    no tranc

--- src/main/java/com/handysoft/service/UserDataService.java
+++ src/main/java/com/handysoft/service/UserDataService.java
@@ -1,18 +1,14 @@
 package com.handysoft.service;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import com.handysoft.model.UserInfo;
 import com.handysoft.model.UserExtraInfo;
+import com.handysoft.model.UserInfo;
 import com.handysoft.repository.UserBeanRepository;
 import com.handysoft.repository.UserExtraBeanRepository;
 
 @Service
-@Transactional
 public class UserDataService {
 	
 	
