d1d9fa7eca76f774fb27f5436609c837774a8ae6
partnerJun <partner_jun@live.co.kr>
2016-02-11 10:00:03 +0900

    jpaRepository

--- src/main/java/com/handysoft/repository/SensingValueRepository.java
+++ src/main/java/com/handysoft/repository/SensingValueRepository.java
@@ -2,14 +2,14 @@ package com.handysoft.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.handysoft.model.SIHMSSensingData;
 import com.handysoft.model.SensingDataCompositeKey;
 
 @Repository
-public interface SensingValueRepository extends CrudRepository<SIHMSSensingData, SensingDataCompositeKey> {
+public interface SensingValueRepository extends JpaRepository<SIHMSSensingData, SensingDataCompositeKey> {
 
 	List<SIHMSSensingData> findBySeqAndYearAndMonthAndDayOrderByLogDateAsc(int seq, int year, int month, int day);
 
